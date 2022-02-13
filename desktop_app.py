be647a89f04f532a507aec84b474f0eaa2c786fe
BahadirAkdemir <akdemir_bahadir@hotmail.com>
2022-02-14 00:46:19 +0300

    Adding to  Class

--- desktop_app.py
+++ desktop_app.py
@@ -3,38 +3,43 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow,self).__init__()
+        self.setWindowTitle("PyQt5 Application")
+        self.setGeometry(200,200,500,500)
+        self.InitUI()
+
+    def InitUI(self):
+        self.label_name = QtWidgets.QLabel(self)
+        self.label_name.setText("Product Name")
+        self.label_name.move(20,20)
+        self.text_name = QtWidgets.QLineEdit(self)
+        self.text_name.move(120,20)
+
+        self.label_price = QtWidgets.QLabel(self)
+        self.label_price.setText("Price")
+        self.label_price.move(20,80)
+        self.text_price = QtWidgets.QLineEdit(self)
+        self.text_price.move(120,80)
+
+        button_save = QtWidgets.QPushButton(self)
+        button_save.setText("Save")
+        button_save.move(120,140)
+
+        self.label_result = QtWidgets.QLabel(self)
+        self.label_result.setText("Sonuç")
+        self.label_result.resize(300,30)
+        self.label_result.move(120,200)
+
+        button_save.clicked.connect(self.clicked)
+
+    def clicked(self):
+        print(self.label_result.setText(f"Product Name:{self.text_name.text()} \nPrice: ${self.text_price.text()}"))
 
 def window():
     app=QtWidgets.QApplication(sys.argv)
-    win=QtWidgets.QMainWindow()
-    win.setWindowTitle("PyQt5 Application")
-    win.setGeometry(200,200,500,500)
-
-    label_name = QtWidgets.QLabel(win)
-    label_name.setText("Product Name")
-    label_name.move(20,20)
-    text_name = QtWidgets.QLineEdit(win)
-    text_name.move(120,20)
-
-    label_price = QtWidgets.QLabel(win)
-    label_price.setText("Price")
-    label_price.move(20,80)
-    text_price = QtWidgets.QLineEdit(win)
-    text_price.move(120,80)
-
-    button_save = QtWidgets.QPushButton(win)
-    button_save.setText("Save")
-    button_save.move(120,140)
-
-    def clicked():
-        print("Product Name:",text_name.text(),"\nPrice: $",text_price.text())
-
-    button_save.clicked.connect(clicked)
-
-
-
-
-
+    win=MainWindow()
     win.show()
     sys.exit(app.exec_())
 
